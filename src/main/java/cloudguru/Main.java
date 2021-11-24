6ce27cb7eca88ce3e9bf531b7402ca88468d7d5b
Larissa JustoEvaldt <larissaj@amdocs.com>
2021-11-24 09:44:44 +0000

    first commit

---
+++ src/main/java/cloudguru/Main.java
@@ -0,0 +1,9 @@
+package cloudguru;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MemberSignupsConsumer memberSignupsConsumer = new MemberSignupsConsumer();
+        memberSignupsConsumer.run();
+    }
+}
---
+++ src/main/java/confluent/ConsumerRecordsHandler.java
@@ -0,0 +1,7 @@
+package confluent;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+public interface ConsumerRecordsHandler<K, V> {
+    void process(ConsumerRecords<K, V> consumerRecords);
+}
